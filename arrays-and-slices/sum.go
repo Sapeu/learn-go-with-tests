8f98e50700a064737ac1a79e7067f9479f86eabe
Sapeu <sapeu.lee@foxmail.com>
2020-07-26 00:33:34 +0800

    切片 SumAll

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -7,3 +7,14 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	// lengthOfNumbers := len(numbersToSum)
+	// sums = make([]int, lengthOfNumbers)
+	for _, numbers := range numbersToSum {
+		// sums[i] = Sum(numbers)
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,9 +1,13 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	assertCorrectSum := func(t *testing.T, got, want int, numbers []int) {
+		t.Helper()
 		if want != got {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
@@ -22,4 +26,15 @@ func TestSum(t *testing.T) {
 		want := 6
 		assertCorrectSum(t, got, want, numbers)
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	// 尝试比较 slice 和 string。这显然是不合理的，但是却通过了编译！所以使用 reflect.DeepEqual 比较简洁但是在使用时需多加小心
+	// want := "bob"
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
