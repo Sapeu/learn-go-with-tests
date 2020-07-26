c07f622b6ec11f7a2a603e99f83256dfd50b6868
Sapeu <sapeu.lee@foxmail.com>
2020-07-27 00:01:09 +0800

    reflection walk struct array map slice

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -4,15 +4,35 @@ import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
 	// fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
-	val := reflect.ValueOf(x)
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-		if field.Kind() == reflect.String {
-			fn(field.String())
-		}
+	val := getValue(x)
 
-		if field.Kind() == reflect.Struct {
-			walk(field.Interface(), fn)
+	numberOfValues := 0
+	var getFeild func(int) reflect.Value
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getFeild = val.Field
+	case reflect.Slice, reflect.Array:
+		numberOfValues = val.Len()
+		getFeild = val.Index
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
 		}
 	}
+
+	for i := 0; i < numberOfValues; i++ {
+		walk(getFeild(i).Interface(), fn)
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	return val
 }
