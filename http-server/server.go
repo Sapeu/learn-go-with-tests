c0dfeff5dd7bb76c5fe2d884d941d9376b58b74c
Sapeu <sapeu.lee@foxmail.com>
2020-07-27 18:04:19 +0800

    StubPlayerStore

--- http-server/server.go
+++ http-server/server.go
@@ -5,19 +5,36 @@ import (
 	"net/http"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	player := r.URL.Path[len("/players/"):]
-
-	fmt.Fprint(w, GetPlayerScore(player))
+type PlayerStore interface {
+	GetPlayerScore(name string) int
 }
 
-func GetPlayerScore(name string) string {
-	if name == "Peper" {
-		return "20"
-	}
+type PlayerServer struct {
+	store PlayerStore
+}
 
-	if name == "Floyd" {
-		return "10"
-	}
-	return ""
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
+
+// func GetPlayerScore(name string) int {
+// 	if name == "Peper" {
+// 		return 20
+// 	}
+
+// 	if name == "Floyd" {
+// 		return 10
+// 	}
+// 	return 0
+// }
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
 }
