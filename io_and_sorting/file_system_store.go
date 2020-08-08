9109b252a81da509a00ab30c8ef6aee2126af8a2
Sapeu <sapeu.lee@foxmail.com>
2020-08-09 00:01:09 +0800

    initialisePlayerDBFile

--- io_and_sorting/file_system_store.go
+++ io_and_sorting/file_system_store.go
@@ -36,15 +36,9 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 }
 
 func NewFileSystemPlayerStore(database *os.File) (*FileSystemPlayerStore, error) {
-	database.Seek(0, 0)
-	info, err := database.Stat()
+	err := initialisePlayerDBFile(database)
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", database.Name(), err)
-	}
-
-	if info.Size() == 0 {
-		database.Write([]byte("[]"))
-		database.Seek(0, 0)
+		return nil, fmt.Errorf("problem initialising player db file, %v", err)
 	}
 
 	league, err := NewLeague(database)
@@ -52,4 +46,18 @@ func NewFileSystemPlayerStore(database *os.File) (*FileSystemPlayerStore, error)
 		return nil, fmt.Errorf("problem loading player store from file %s, %v", database.Name(), err)
 	}
 	return &FileSystemPlayerStore{database: json.NewEncoder(&tape{database}), league: league}, nil
+}
+
+func initialisePlayerDBFile(file *os.File) error {
+	file.Seek(0, 0)
+	info, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
+	return nil
 }
