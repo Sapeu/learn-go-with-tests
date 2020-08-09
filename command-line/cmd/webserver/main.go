805ccbdf87c56c19b02a8a245f0f4ddb16204146
Sapeu <sapeu.lee@foxmail.com>
2020-08-09 18:21:45 +0800

    FileSystemPlayerStoreFromFile

--- command-line/cmd/webserver/main.go
+++ command-line/cmd/webserver/main.go
@@ -4,19 +4,14 @@ import (
 	poker "learn-go-with-tests/command-line"
 	"log"
 	"net/http"
-	"os"
 )
 
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
-	}
-	store, err := poker.NewFileSystemPlayerStore(db)
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v ", err)
+		log.Fatal(err)
 	}
 	server := poker.NewPlayerServer(store)
 	if err := http.ListenAndServe(":5000", server); err != nil {
