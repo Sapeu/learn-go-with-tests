e2ae0e313e5af8d8a01c31f8119486df4b67cc53
Sapeu <sapeu.lee@foxmail.com>
2020-08-09 18:03:25 +0800

    cli test record cleo win from user input

--- command-line/cli_test.go
+++ command-line/cli_test.go
@@ -9,9 +9,17 @@ func TestCLI(t *testing.T) {
 	t.Run("record chris win from user input", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
 		playerStore := &StubPlayerStore{}
-		cli := &CLI{playerStore, in}
+		cli := NewCLI(playerStore, in)
 		cli.PlayPoker()
 		assertPlayerWin(t, playerStore, "Chris")
+	})
+
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &StubPlayerStore{}
+		cli := NewCLI(playerStore, in)
+		cli.PlayPoker()
+		assertPlayerWin(t, playerStore, "Cleo")
 	})
 }
 
